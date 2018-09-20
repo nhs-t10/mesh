b64517488d7ff5ce4eb4a0e616b6882b2ca83bd7
uniquenamesarehard <31969760+uniquenamesarehard@users.noreply.github.com>
2018-09-20 15:49:38 -0400

    basic mecanum drive

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tSuper.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tSuper.java
@@ -1,9 +1,45 @@
 package org.firstinspires.ftc.teamcode;
-
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+//import com.qualcomm.robotcore.hardware.Servo;
 
 public abstract class tSuper extends OpMode
 {
 
+    public DcMotor leftF, leftB, rightF, rightB;
+    //public Servo
 
+
+
+    public void init() {
+        leftF = hardwareMap.dcMotor.get("frontLeft");
+        rightF = hardwareMap.dcMotor.get("frontRight");
+        leftB = hardwareMap.dcMotor.get("backLeft");
+        rightB = hardwareMap.dcMotor.get("backRight");
+
+        //rightF.setDirection(DcMotor.Direction.REVERSE);
+        //rightB.setDirection(DcMotor.Direction.REVERSE);
+
+        prepare();
+
+    }
+
+    public abstract void prepare();
+
+    public void drive(double left, double right) {
+        leftF.setPower(left);
+        leftB.setPower(left);
+        rightF.setPower(right);
+        rightB.setPower(right);
+
+    }
+
+
+    public void side(double side)
+    {
+        leftF.setPower(-side);
+        leftB.setPower(side);
+        rightF.setPower(side);
+        rightB.setPower(-side);
+    }
 }
--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp.java
@@ -1,9 +1,26 @@
 package org.firstinspires.ftc.teamcode;
-
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@TeleOp(name = "teleOp")
 public class teleOp extends tSuper
 {
+    public void prepare()
+    {}
+
     public void loop()
     {
-        drive()
+        double linear = gamepad1.left_stick_y;
+        double rotation = gamepad1.left_stick_x;
+        double side = gamepad1.right_stick_x;
+
+        if (side == 0) {
+            drive(.7*(linear - rotation), .7*(linear + rotation));
+        } else {
+            side(.7*side);
+        }
     }
+
+    public void stop()
+    {}
+
 }
