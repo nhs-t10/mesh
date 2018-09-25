83424bc6c793ddfc0286b005ae30a2bdaf93087c
uniquenamesarehard <31969760+uniquenamesarehard@users.noreply.github.com>
2018-09-25 19:04:41 -0400

    commenting

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp.java
@@ -1,5 +1,5 @@
 package org.firstinspires.ftc.teamcode;
-import com.qualcomm.robotcore.util.Range;
+//import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name = "teleOp")
 public class teleOp extends tSuper
@@ -12,14 +12,16 @@ public class teleOp extends tSuper
         double linear = gamepad1.left_stick_y;
         double rotation = gamepad1.left_stick_x;
         double side = gamepad1.right_stick_x;
+        //defining the stuff
 
-        if (side == 0) {
+        if (side == 0)
+        {
             drive(.7*(linear - rotation), .7*(linear + rotation));
         } else {
             side(.7*side);
         }
     }
-
+        //linear = straight, rotation = turning, side = skating. Linear - rotation will compensate one side to allow the other side to overrotate
     public void stop()
     {}
 
