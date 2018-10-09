a764a4a8e4161cbca6f80fecd3c0c931f1822795
Davis Keene <dbkeene.tsyc@gmail.com>
2018-10-09 19:39:44 -0400

    Driving fixed!

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp.java
@@ -14,8 +14,8 @@ public class teleOp extends T10_Library
 
     public void loop() {
         float linear = gamepad1.left_stick_y;
-        float rotation = gamepad1.right_stick_x;
-        float side = gamepad1.left_stick_x;
+        float side = gamepad1.right_stick_x;
+        float rotation = gamepad1.left_stick_x;
         //defining the stuff. linear = straight, rotation = turning, side = skating.
         //Linear - rotation will compensate one side to allow the other side to overrotate
 
