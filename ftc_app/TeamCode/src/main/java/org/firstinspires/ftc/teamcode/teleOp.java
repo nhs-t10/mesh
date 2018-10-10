b1f19521ba6ec12a9ac91ab436a2753df2aa9554
Davis Keene <dbkeene.tsyc@gmail.com>
2018-10-09 22:26:10 -0400

    Driving fixed! (had to invert side)

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp.java
@@ -14,12 +14,12 @@ public class teleOp extends T10_Library
 
     public void loop() {
         float linear = gamepad1.left_stick_y;
-        float side = gamepad1.right_stick_x;
+        float side = -gamepad1.right_stick_x;
         float rotation = gamepad1.left_stick_x;
         //defining the stuff. linear = straight, rotation = turning, side = skating.
         //Linear - rotation will compensate one side to allow the other side to overrotate
 
-        omni(linear, rotation, -side);
+        omni(linear, rotation, side);
 
         telemetry.addData("left_y",linear);
         telemetry.addData("right_x",linear);
