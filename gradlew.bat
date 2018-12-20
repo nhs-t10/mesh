7bda601eed70abe713333ee5bbc6b5d803de535a
TheGreatRambler <aehart684351@gmail.com>
2018-12-20 00:04:57 -0500

    hi

--- gradlew.bat
+++ gradlew.bat
@@ -12,7 +12,7 @@ if "%OS%"=="Windows_NT" setlocal
 set DEFAULT_JVM_OPTS=
 
 set DIRNAME=%~dp0
-if "%DIRNAME%" == "" set DIRNAME=.
+if "%DIRNAME%" == "" set DIRNAME=. 
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
